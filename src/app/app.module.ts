b305b9882d512de566d141a8318ef5c4177233d7
Andrés Reverón Molina <andressser@gmail.com>
2019-06-01 20:36:14 +0200

    update angular material 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { PortalModule } from "@angular/cdk/portal";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatButtonModule } from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from "@angular/material/input";
 import { AppComponent } from "./app.component";
 import { PortalComponent } from "./components/portal/portal.component";
